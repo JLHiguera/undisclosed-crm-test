1dd3e695a5a511c93b521acce7dfb843e2e1fbe2
José Luis A. Higuera <jose@higuera.dev>
2024-04-06 14:54:07 -0600

    Fix test suite for clients as well as adding new tests

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -2,45 +2,78 @@
 
 namespace Tests\Feature;
 
+use App\Models\Client;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ClientTest extends TestCase
 {
-
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     */
+    /** @test */
     public function create_client_test(): void
     {
         $client_data = [
             'nombre' => 'Test',
-            'razon_social' => '',
+            'razon_social' => 'AAAABBBBCD',
             'direccion' => 'Padre Mier',
-            'estimacion_venta' => '200.00',
+            'estimacion_venta' => 200.07,
             'observaciones' => 'ninguna',
             'giro' => 'tecnología',
             'url' => 'http://localhost',
             'fase_venta' => 'iniciada',
         ];
 
-        $response = $this->postJson('/api/client', $client_data)
+        $response = $this->postJson('/api/clients', $client_data)
             ->assertCreated();
+    }
+
+    /** @test */
+    public function list_clients(): void 
+    {
+        Client::factory()->count(15)->create();
+
+        $this->get('/api/clients')
+            ->assertJsonCount(15, 'data');
+    }
+
+    /** @test */
+    public function list_only_active_clients(): void 
+    {
+        $total_client_count = 15;
+
+        $clients = Client::factory()->count($total_client_count)->create();
+
+        $this->get('/api/clients')
+            ->assertJsonCount($total_client_count, 'data');
+
+        $deleted_count = 0;
+
+        foreach($clients as $key => $client) {
+            if ($key % 2 == 0) {
+                $deleted_count += 1;
+                $this->delete("/api/clients/{$client->id}");
+            }
+        }
+
+        $total_active = $total_client_count - $deleted_count;
+
+        $this->get('/api/clients')
+            ->assertJsonCount($total_active, 'data');
     }
 
     /**
      * Test the retrieval of a single client.
      */
+    /** @test */
     public function get_single_client_info(): void
     {
         $client_data = [
             'nombre' => 'Test',
-            'razon_social' => '',
+            'razon_social' => 'AAAABBBBCD',
             'direccion' => 'Padre Mier',
-            'estimacion_venta' => '200.00',
+            'estimacion_venta' => 200.57,
             'observaciones' => 'ninguna',
             'giro' => 'tecnología',
             'url' => 'http://localhost',
@@ -53,40 +86,44 @@ class ClientTest extends TestCase
             ->assertJsonFragment($client_data);
     }
 
+    /** @test */
     public function edit_client_info(): void
     {
         $client_data = [
             'nombre' => 'Test',
+            'razon_social' => 'AAAABBBBCD',
             'direccion' => 'Padre Mier',
-            'estimacion_venta' => '200.00',
+            'estimacion_venta' => 123.45,
             'observaciones' => 'ninguna',
             'giro' => 'tecnología',
             'url' => 'http://localhost',
             'fase_venta' => 'iniciada',
         ];
 
-        $client = Clients::create($client_data);
+        $client = Client::create($client_data);
 
         $expected_data = [
             'nombre' => 'Test',
+            'razon_social' => 'AAAABBBBCD',
             'direccion' => 'Padre Mier',
-            'estimacion_venta' => '400.00',
+            'estimacion_venta' => 543.21,
             'observaciones' => 'ninguna',
             'giro' => 'logistica',
             'url' => 'http://localhost',
             'fase_venta' => 'iniciada',
         ];
 
-        $this->putJson("/api/client/{$client->id}", $client_data);
+        $this->patchJson("/api/clients/{$client->id}", $expected_data);
 
-        $this->get("/api/client/{$client->id}")
-            ->assertStatus('200')
+        $this->get("/api/clients/{$client->id}")
+            ->assertStatus(200)
             ->assertJsonFragment($expected_data);
     }
 
+    /** @test */
     public function delete_client(): void
     {
-        $client = Client::factory()->take(1);
+        $client = Client::factory()->create();
 
         $this->assertDatabaseHas('clients', [
             'id' => $client->id,
